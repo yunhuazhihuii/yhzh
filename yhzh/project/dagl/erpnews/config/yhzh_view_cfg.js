5248cf406c99b98112e8dc7d0d5d5cdca64f79a0
Dhpong <34147965+Dhpong@users.noreply.github.com>
2019-03-18 10:50:36 +0800

    delect

--- yhzh/project/dagl/erpnews/config/yhzh_view_cfg.js
+++
@@ -1,9 +0,0 @@
-
-//界面工程配置文件 chenms 2019-2-6
-export const viewConfig = {
-	
-    // restUrl : 'http://116.62.112.118:8888/api/'     //连接后台restful接口url   //服务器
-    // restUrl : 'http://192.168.1.187:8888/api/'    //邓城本地  192.168.0.109
-
-    restUrl : 'http://192.168.0.122:8888/api/' 
-};
